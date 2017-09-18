1adac03f0e581ed2f393a01b9c4a3eaf6aaa1174
ndsnas <ndsnas_007@yahoo.com>
2017-09-18 22:46:10 +0530

    flash added!

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,42 +1,30 @@
 class ManagerController < ApplicationController
   def add_student
-
-
-
   if request.get?
   @student = Student.new
   end
-
 if request.post?
   @student = Student.new(student_params)
    if @student.save
-
     flash[:notice] = "Successfully created..."
+    redirect_to(manager_add_student_path)
 end
-#  else
-#
  end
-
-
-  end
+end
 
 
   def delete_student
-
     if request.get?
       @student = Student.new
-      #@student = Student.find(params[:id])
     end
-
-
-
 if request.post?
     @student = Student.find_by_roll_no(params[:student][:roll_no])
   @student.destroy
   flash[:notice] = "Successfully deleted '#{@student.name}'..."
-#  redirect_to(subjects_path)
+  redirect_to(manager_delete_student_path)
 end
   end
+
 
   def view_menu
     if request.get?
@@ -47,27 +35,15 @@ end
   end
   end
 
+
   def update_menu
-
-    #if request.get?
-      #@menus = Menu.all
         @menu = Menu.find(params[:id])
-    #end
-
     if request.patch?
-    #  @menu = Menu.find(params[:id])
-      @menu.update_attributes(menu_params)
+    if  @menu.update_attributes(menu_params)
+      flash[:notice] = "Successfully updated menu ... "
+      redirect_to(manager_view_menu_path)
     end
-
-
-
-
-
-
-  #  @menu = Menu.find(params[:id])
-
-  # @menu.update_attributes(menu_params)
-
+    end
   end
 
   def add_mess_cut
